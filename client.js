9cb26a239741cc6a1d26f3a08a90a96334a4718e
Minh Nguyen <minhna@gmail.com>
2025-03-19 00:40:33 +0700

    replace Accounts._findUserByQuery function

--- client.js
+++ client.js
@@ -1,63 +1,62 @@
 /* globals twoFactor */
 
-const state = new ReactiveDict('twoFactor');
+import { Accounts } from 'meteor/accounts-base'
+import { Meteor } from 'meteor/meteor'
+import { ReactiveDict } from 'meteor/reactive-dict'
 
-state.set('user', '');
-state.set('password', '');
-state.set('verifying', false);
+const state = new ReactiveDict('twoFactor')
 
-const getSelector = user => {
+state.set('user', '')
+state.set('password', '')
+state.set('verifying', false)
+
+const getSelector = (user) => {
   if (typeof user === 'string') {
     if (user.indexOf('@') === -1) {
-      return { username: user };
+      return { username: user }
     }
-    return { email: user };
+    return { email: user }
   }
-  return user;
-};
+  return user
+}
 
 const callbackHandler = (cb, handlerCb) => {
-  return error => {
+  return (error) => {
     if (error) {
-      return typeof cb === 'function' && cb(error);
+      return typeof cb === 'function' && cb(error)
     }
 
     if (typeof handlerCb === 'function') {
-      handlerCb();
+      handlerCb()
     }
 
-    return typeof cb === 'function' && cb();
-  };
-};
+    return typeof cb === 'function' && cb()
+  }
+}
 
 const getAuthCode = (user, password, cb) => {
-  const selector = getSelector(user);
-  const hashedPassword = Accounts._hashPassword(password);
+  const selector = getSelector(user)
+  const hashedPassword = Accounts._hashPassword(password)
 
   const callback = callbackHandler(cb, () => {
-    state.set('verifying', true);
-    state.set('user', user);
-    state.set('password', hashedPassword);
-  });
+    state.set('verifying', true)
+    state.set('user', user)
+    state.set('password', hashedPassword)
+  })
 
-  Meteor.call(
-    'twoFactor.getAuthenticationCode',
-    selector,
-    hashedPassword,
-    callback,
-  );
-};
+  Meteor.call('twoFactor.getAuthenticationCode', selector, hashedPassword, callback)
+}
 
-const getNewAuthCode = cb => {
-  const selector = getSelector(state.get('user'));
-  const password = state.get('password');
-  const callback = callbackHandler(cb);
+const getNewAuthCode = (cb) => {
+  const selector = getSelector(state.get('user'))
+  const password = state.get('password')
+  const callback = callbackHandler(cb)
 
-  Meteor.call('twoFactor.getAuthenticationCode', selector, password, callback);
-};
+  Meteor.call('twoFactor.getAuthenticationCode', selector, password, callback)
+}
 
 const verifyAndLogin = (code, cb) => {
-  const selector = getSelector(state.get('user'));
+  const selector = getSelector(state.get('user'))
 
   Accounts.callLoginMethod({
     methodName: 'twoFactor.verifyCodeAndLogin',
@@ -69,32 +68,32 @@ const verifyAndLogin = (code, cb) => {
       },
     ],
     userCallback: callbackHandler(cb, () => {
-      state.set('verifying', false);
-      state.set('user', '');
-      state.set('password', '');
+      state.set('verifying', false)
+      state.set('user', '')
+      state.set('password', '')
     }),
-  });
-};
+  })
+}
 
-const isVerifying = () => state.get('verifying');
+const isVerifying = () => state.get('verifying')
 
-const abort = cb => {
-  const selector = getSelector(state.get('user'));
-  const password = state.get('password');
+const abort = (cb) => {
+  const selector = getSelector(state.get('user'))
+  const password = state.get('password')
 
   const callback = callbackHandler(cb, () => {
     state.set({
       verifying: false,
       user: '',
       password: '',
-    });
-  });
+    })
+  })
 
-  Meteor.call('twoFactor.abort', selector, password, callback);
-};
+  Meteor.call('twoFactor.abort', selector, password, callback)
+}
 
-twoFactor.getAuthCode = getAuthCode;
-twoFactor.getNewAuthCode = getNewAuthCode;
-twoFactor.verifyAndLogin = verifyAndLogin;
-twoFactor.isVerifying = isVerifying;
-twoFactor.abort = abort;
+twoFactor.getAuthCode = getAuthCode
+twoFactor.getNewAuthCode = getNewAuthCode
+twoFactor.verifyAndLogin = verifyAndLogin
+twoFactor.isVerifying = isVerifying
+twoFactor.abort = abort
--- server.js
+++ server.js
@@ -1,158 +1,173 @@
-import { Match, check } from "meteor/check";
+import { Accounts } from 'meteor/accounts-base'
+import { Match, check } from 'meteor/check'
+import { Meteor } from 'meteor/meteor'
 
 /* globals twoFactor */
 
-twoFactor.options = {};
+twoFactor.options = {}
 
 const generateCode = () => {
   return Array(...Array(6))
     .map(() => {
-      return Math.floor(Math.random() * 10);
+      return Math.floor(Math.random() * 10)
     })
-    .join("");
-};
+    .join('')
+}
 
 const NonEmptyString = Match.Where((x) => {
-  check(x, String);
-  return x.length > 0;
-});
+  check(x, String)
+  return x.length > 0
+})
 
 const userQueryValidator = Match.Where((user) => {
   check(user, {
     id: Match.Optional(NonEmptyString),
     username: Match.Optional(NonEmptyString),
     email: Match.Optional(NonEmptyString),
-  });
+  })
   if (Object.keys(user).length !== 1) {
-    throw new Match.Error("User property must have exactly one field");
+    throw new Meteor.Error('User property must have exactly one field')
   }
-  return true;
-});
+  return true
+})
 
-const passwordValidator = { digest: String, algorithm: String };
+const passwordValidator = { digest: String, algorithm: String }
 
 const invalidLogin = () => {
-  return new Meteor.Error(403, "Invalid login credentials");
-};
+  return new Meteor.Error(403, 'Invalid login credentials')
+}
 
 const getFieldName = () => {
-  return twoFactor.options.fieldName || "twoFactorCode";
-};
+  return twoFactor.options.fieldName || 'twoFactorCode'
+}
 
 Meteor.methods({
-  async "twoFactor.getAuthenticationCode"(userQuery, password) {
-    check(userQuery, userQueryValidator);
-    check(password, passwordValidator);
+  async 'twoFactor.getAuthenticationCode'(userQuery, password) {
+    check(userQuery, userQueryValidator)
+    check(password, passwordValidator)
 
-    const fieldName = getFieldName();
+    const fieldName = getFieldName()
 
-    const user = await Accounts._findUserByQuery(userQuery);
-    if (!user) {
-      throw invalidLogin();
+    let user
+    if (userQuery.username) {
+      user = await Accounts.findUserByUsername(userQuery.username)
+    } else if (userQuery.email) {
+      user = await Accounts.findUserByEmail(userQuery.email)
     }
 
-    const checkPassword = await Accounts._checkPasswordAsync(user, password);
+    if (!user) {
+      throw invalidLogin()
+    }
+
+    const checkPassword = await Accounts._checkPasswordAsync(user, password)
     if (checkPassword.error) {
-      throw invalidLogin();
+      throw invalidLogin()
     }
 
     const code =
-      typeof twoFactor.generateCode === "function"
+      typeof twoFactor.generateCode === 'function'
         ? twoFactor.generateCode()
-        : generateCode();
+        : generateCode()
 
-    if (typeof twoFactor.sendCode === "function") {
-      await twoFactor.sendCode(user, code);
+    if (typeof twoFactor.sendCode === 'function') {
+      await twoFactor.sendCode(user, code)
     }
 
     await Meteor.users.updateAsync(user._id, {
       $set: {
         [fieldName]: code,
       },
-    });
+    })
   },
-  async "twoFactor.verifyCodeAndLogin"(options) {
+  async 'twoFactor.verifyCodeAndLogin'(options) {
     check(options, {
       user: userQueryValidator,
       password: passwordValidator,
       code: String,
-    });
+    })
 
-    const fieldName = getFieldName();
+    const fieldName = getFieldName()
 
-    const user = await Accounts._findUserByQuery(options.user);
+    let user
+    if (options.user.username) {
+      user = await Accounts.findUserByUsername(options.user.username)
+    } else if (options.user.email) {
+      user = await Accounts.findUserByEmail(options.user.email)
+    }
     if (!user) {
-      throw invalidLogin();
+      throw invalidLogin()
     }
 
-    const checkPassword = await Accounts._checkPasswordAsync(
-      user,
-      options.password
-    );
+    const checkPassword = await Accounts._checkPasswordAsync(user, options.password)
     if (checkPassword.error) {
-      throw invalidLogin();
+      throw invalidLogin()
     }
 
     if (options.code !== user[fieldName]) {
-      throw new Meteor.Error(403, "Invalid code");
+      throw new Meteor.Error(403, 'Invalid code')
     }
 
     Meteor.users.updateAsync(user._id, {
       $unset: {
-        [fieldName]: "",
+        [fieldName]: '',
       },
-    });
+    })
 
-    return await Accounts._attemptLogin(this, "login", "", {
-      type: "2FALogin",
+    return await Accounts._attemptLogin(this, 'login', '', {
+      type: '2FALogin',
       userId: user._id,
-    });
+    })
   },
-  async "twoFactor.abort"(userQuery, password) {
-    check(userQuery, userQueryValidator);
-    check(password, passwordValidator);
+  async 'twoFactor.abort'(userQuery, password) {
+    check(userQuery, userQueryValidator)
+    check(password, passwordValidator)
 
-    const fieldName = getFieldName();
+    const fieldName = getFieldName()
 
-    const user = await Accounts._findUserByQuery(userQuery);
+    let user
+    if (userQuery.username) {
+      user = await Accounts.findUserByUsername(userQuery.username)
+    } else if (userQuery.email) {
+      user = await Accounts.findUserByEmail(userQuery.email)
+    }
     if (!user) {
-      throw invalidLogin();
+      throw invalidLogin()
     }
 
-    const checkPassword = await Accounts._checkPasswordAsync(user, password);
+    const checkPassword = await Accounts._checkPasswordAsync(user, password)
     if (checkPassword.error) {
-      throw invalidLogin();
+      throw invalidLogin()
     }
 
     await Meteor.users.updateAsync(user._id, {
       $unset: {
-        [fieldName]: "",
+        [fieldName]: '',
       },
-    });
+    })
   },
-});
+})
 
 Accounts.validateLoginAttempt((options) => {
   const customValidator = () => {
-    if (typeof twoFactor.validateLoginAttempt === "function") {
-      return twoFactor.validateLoginAttempt(options);
+    if (typeof twoFactor.validateLoginAttempt === 'function') {
+      return twoFactor.validateLoginAttempt(options)
     }
-    return false;
-  };
+    return false
+  }
 
-  const allowedMethods = ["createUser", "resetPassword", "verifyEmail"];
+  const allowedMethods = ['createUser', 'resetPassword', 'verifyEmail']
 
   if (
     customValidator() ||
-    options.type === "resume" ||
+    options.type === 'resume' ||
     allowedMethods.indexOf(options.methodName) !== -1
   ) {
-    return true;
+    return true
   }
 
-  if (options.type === "2FALogin" && options.methodName === "login") {
-    return options.allowed;
+  if (options.type === '2FALogin' && options.methodName === 'login') {
+    return options.allowed
   }
 
-  return false;
-});
+  return false
+})
